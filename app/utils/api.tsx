43faccf2134695f635e009bc357cef8c6578c285
hayato33 <country.devide1933@gmail.com>
2024-11-21 22:12:54 +0900

    修正

--- app/utils/api.tsx
+++ app/utils/api.tsx
@@ -1,11 +1,11 @@
-const request = (path: string, method = 'GET', body: any = undefined, token: string) => {
+const request = (path: string, method = 'GET', body: Record<string, unknown> | null = null, token: string) => {
   return fetch(path, {
     method: method,
     headers: {
       'Content-Type': 'application/json',
       Authorization: token,
     },
-    body: body,
+    body: body ? JSON.stringify(body) : null,
   });
 };
 export default request;
