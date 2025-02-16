074523f152006a325f75ccca185f26f3874d89c3
hayato33 <country.devide1933@gmail.com>
2025-02-16 16:09:29 +0900

    Footerコンポーネント作成

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className='fixed bottom-0 left-0 right-0 grid place-content-center p-2'>
+      <small>&copy; 2025 Hayato Kokubun</small>
+    </footer>
+  );
+}
