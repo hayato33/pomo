73a70a22e329ddf1c894256318d1e2cc3d8884d3
hayato33 <country.devide1933@gmail.com>
2025-03-10 17:17:55 +0900

    ユーザー設定のデフォルト値作成

---
+++ src/app/_config/userSettingConfig.ts
@@ -0,0 +1,17 @@
+/** ユーザー設定のデフォルト値 */
+export const DEFAULT_USER_SETTINGS = {
+  autoStartShortBreak: false,
+  autoStartFocusTime: false,
+  autoStartLongBreak: false,
+  focusTimeSound: "noSound", //仮の値
+  shortBreakSound: "noSound", //仮の値
+  longBreakSound: "noSound", //仮の値
+  soundVolume: 50,
+  timelinePageLink: true,
+  postButtonToTimeline: true,
+  rankingPageLink: true,
+  showOnRanking: false,
+  font: "font01", //仮の値
+  hideExplainText: false,
+  setRandomTime: false,
+};
