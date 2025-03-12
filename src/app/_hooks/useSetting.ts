1b1ddc534ced81c930014e03a90d07cb72558c75
hayato33 <country.devide1933@gmail.com>
2025-03-12 09:13:41 +0900

    useGetEndpoint（token取得を含む汎用的なAPIデータ取得カスタムフック）を作成し、useUser と useSetting を簡素化

--- src/app/_hooks/useSetting.ts
+++ src/app/_hooks/useSetting.ts
@@ -1,20 +1,9 @@
-import useSWR from "swr";
-import { fetcher } from "../_utils/fetcher";
+import { useGetEndpoint } from "./useGetEndpoint";
 
 /**
  * ユーザー設定を取得するカスタムフック
- * @param token - 認証トークン
- * @returns ユーザー情報、ローディング状態、エラー状態、データ再取得関数
+ * @returns ユーザー設定、ローディング状態、エラー状態、データ再取得関数
  */
-export const useSetting = ({ token }: { token: string }) => {
-  const { data, error, isLoading, mutate } = useSWR("/api/setting", (apiPath) =>
-    fetcher({ apiPath, token })
-  );
-
-  return {
-    setting: data,
-    isLoading,
-    isError: error,
-    mutate,
-  };
+export const useSetting = () => {
+  return useGetEndpoint("/api/setting");
 };
--- src/app/_hooks/useUser.ts
+++ src/app/_hooks/useUser.ts
@@ -1,20 +1,9 @@
-import useSWR from "swr";
-import { fetcher } from "../_utils/fetcher";
+import { useGetEndpoint } from "./useGetEndpoint";
 
 /**
  * ユーザー情報を取得するカスタムフック
- * @param token - 認証トークン
  * @returns ユーザー情報、ローディング状態、エラー状態、データ再取得関数
  */
-export const useUser = ({ token }: { token: string }) => {
-  const { data, error, isLoading, mutate } = useSWR("/api/user", (apiPath) =>
-    fetcher({ apiPath, token })
-  );
-
-  return {
-    user: data,
-    isLoading,
-    isError: error,
-    mutate,
-  };
+export const useUser = () => {
+  return useGetEndpoint("/api/user");
 };
