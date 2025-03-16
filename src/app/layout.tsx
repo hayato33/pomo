cb53cd5a3f51126ad0afdeeffe4ad8ab5b58bf60
hayato33 <country.devide1933@gmail.com>
2025-03-16 23:51:47 +0900

    レイアウトコンポーネントのメインセクションのパディングを調整

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
       >
         <Theme>
           <Header />
-          <main className="px-4 py-12">{children}</main>
+          <main className="px-4 pb-12 pt-4 md:py-12">{children}</main>
           <Footer />
         </Theme>
       </body>
