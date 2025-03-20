1ce8f6be33cbd04585a2aeaab10eb2385bd157f5
hayato33 <country.devide1933@gmail.com>
2025-03-21 03:14:58 +0900

    ポモドーロ記録コンポーネントにタブ機能を追加し、データ表示を週次・月次に切り替え可能に変更

--- src/app/analysis/_components/Record.tsx
+++ src/app/analysis/_components/Record.tsx
@@ -1,78 +1,94 @@
 import ChartCard from "./ChartCard";
-import { SegmentedControl } from "@radix-ui/themes";
-import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
 import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/app/_components/ui/card";
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/app/_components/ui/tabs";
+import AnalysisTabsContent from "./AnalysisTabsContent";
+import { useState } from "react";
 import {
-  type ChartConfig,
-  ChartContainer,
-  ChartTooltip,
-  ChartTooltipContent,
-} from "@/app/_components/ui/chart";
-import AnalysisPomodoroLog from "../_types/analysisPomodoroLog";
-const chartData = [
-  { month: "January", desktop: 186 },
-  { month: "February", desktop: 305 },
-  { month: "March", desktop: 237 },
-  { month: "April", desktop: 73 },
-  { month: "May", desktop: 209 },
-  { month: "June", desktop: 214 },
-];
-
-const chartConfig = {
-  desktop: {
-    label: "Desktop",
-    color: "hsl(var(--chart-1))",
-  },
-} satisfies ChartConfig;
+  createCountChartData,
+  createTimeChartData,
+} from "../_lib/createChartData";
+import { PeriodicPomoData } from "@/app/_types/pomodoro";
 
 interface Props {
-  logs: AnalysisPomodoroLog[];
+  weeklyData: PeriodicPomoData;
+  monthlyData: PeriodicPomoData;
 }
 
-export default function Record({ logs }: Props) {
-  console.log(logs);
+export default function Record({ weeklyData, monthlyData }: Props) {
+  // 期間の状態を管理
+  const [periodType, setPeriodType] = useState<"weekly" | "monthly">("weekly");
+
+  const chartData = {
+    time: {
+      weekly: createTimeChartData(weeklyData),
+      monthly: createTimeChartData(monthlyData),
+    },
+    count: {
+      weekly: createCountChartData(weeklyData),
+      monthly: createCountChartData(monthlyData),
+    },
+  };
 
   return (
-    <ChartCard title="ポモドーロ記録">
-      <div className="absolute right-4 top-4 grid grid-cols-2 gap-4">
-        <SegmentedControl.Root defaultValue="time" size="1">
-          <SegmentedControl.Item value="time">時間</SegmentedControl.Item>
-          <SegmentedControl.Item value="count">回数</SegmentedControl.Item>
-        </SegmentedControl.Root>
-        <SegmentedControl.Root defaultValue="weekly" size="1">
-          <SegmentedControl.Item value="weekly">週間</SegmentedControl.Item>
-          <SegmentedControl.Item value="monthly">月間</SegmentedControl.Item>
-        </SegmentedControl.Root>
-      </div>
-      <Card>
-        <CardHeader>
-          <CardTitle>2025/03/24 - 2025/03/31</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <ChartContainer config={chartConfig}>
-            <BarChart accessibilityLayer data={chartData}>
-              <CartesianGrid vertical={false} />
-              <XAxis
-                dataKey="month"
-                tickLine={false}
-                tickMargin={10}
-                axisLine={false}
-                tickFormatter={(value) => value.slice(0, 3)}
+    <>
+      <ChartCard title="ポモドーロ記録">
+        <Tabs defaultValue="time">
+          <TabsList className="absolute right-36 top-4">
+            <TabsTrigger value="time">時間</TabsTrigger>
+            <TabsTrigger value="count">回数</TabsTrigger>
+          </TabsList>
+          <TabsContent value="time">
+            <Tabs
+              defaultValue={`time-${periodType}`}
+              onValueChange={(value) =>
+                setPeriodType(value.split("-")[1] as "weekly" | "monthly")
+              }
+            >
+              <TabsList className="absolute right-4 top-4">
+                <TabsTrigger value="time-weekly">週間</TabsTrigger>
+                <TabsTrigger value="time-monthly">月間</TabsTrigger>
+              </TabsList>
+              <AnalysisTabsContent
+                value="time-weekly"
+                chartData={chartData.time.weekly}
+                dataKey="time"
               />
-              <ChartTooltip
-                cursor={false}
-                content={<ChartTooltipContent hideLabel />}
+              <AnalysisTabsContent
+                value="time-monthly"
+                chartData={chartData.time.monthly}
+                dataKey="time"
               />
-              <Bar dataKey="desktop" fill="var(--color-desktop)" radius={8} />
-            </BarChart>
-          </ChartContainer>
-        </CardContent>
-      </Card>
-    </ChartCard>
+            </Tabs>
+          </TabsContent>
+          <TabsContent value="count">
+            <Tabs
+              defaultValue={`count-${periodType}`}
+              onValueChange={(value) =>
+                setPeriodType(value.split("-")[1] as "weekly" | "monthly")
+              }
+            >
+              <TabsList className="absolute right-4 top-4">
+                <TabsTrigger value="count-weekly">週間</TabsTrigger>
+                <TabsTrigger value="count-monthly">月間</TabsTrigger>
+              </TabsList>
+              <AnalysisTabsContent
+                value="count-weekly"
+                chartData={chartData.count.weekly}
+                dataKey="count"
+              />
+              <AnalysisTabsContent
+                value="count-monthly"
+                chartData={chartData.count.monthly}
+                dataKey="count"
+              />
+            </Tabs>
+          </TabsContent>
+        </Tabs>
+      </ChartCard>
+    </>
   );
 }
