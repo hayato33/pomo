da5eb16d72ccb67d2a45d3e97fc36de1b4a4ed32
hayato33 <country.devide1933@gmail.com>
2025-03-21 03:14:29 +0900

    分析ページ用のタブコンテンツコンポーネント作成

---
+++ src/app/analysis/_components/AnalysisTabsContent.tsx
@@ -0,0 +1,74 @@
+import { Bar, BarChart, CartesianGrid, LabelList, XAxis } from "recharts";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/app/_components/ui/card";
+import {
+  type ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/app/_components/ui/chart";
+import { TabsContent } from "@/app/_components/ui/tabs";
+
+const chartConfig = {
+  time: {
+    label: "時間(分)",
+  },
+  count: {
+    label: "回数(回)",
+  },
+} satisfies ChartConfig;
+
+interface Props {
+  value: string;
+  chartData:
+    | { date: string; time: number }[]
+    | { date: string; count: number }[];
+  dataKey: "time" | "count";
+}
+
+/** 分析ページのタブコンテンツ */
+export default function AnalysisTabsContent({
+  value,
+  chartData,
+  dataKey,
+}: Props) {
+  return (
+    <TabsContent value={value}>
+      <Card>
+        <CardHeader>
+          <CardTitle className="font-normal">
+            {chartData.length > 0 &&
+              `${chartData[0].date} ～ ${chartData[chartData.length - 1].date}`}
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ChartContainer config={chartConfig}>
+            <BarChart accessibilityLayer data={chartData} margin={{ top: 20 }}>
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey="date"
+                tickLine={false}
+                tickMargin={10}
+                axisLine={false}
+                tickFormatter={(value) => value.split("/")[2]}
+              />
+              <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
+              <Bar dataKey={dataKey} fill="hsl(var(--primary))" radius={8}>
+                <LabelList
+                  position="top"
+                  offset={6}
+                  className="fill-foreground"
+                  fontSize={12}
+                />
+              </Bar>
+            </BarChart>
+          </ChartContainer>
+        </CardContent>
+      </Card>
+    </TabsContent>
+  );
+}
