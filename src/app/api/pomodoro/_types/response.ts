a2bacfb6b78da6fd25d80b1f04176dc718fa7680
hayato33 <country.devide1933@gmail.com>
2025-03-21 02:53:33 +0900

    ポモドーロ統計情報のレスポンス型作成

---
+++ src/app/api/pomodoro/_types/response.ts
@@ -0,0 +1,15 @@
+import { PeriodicPomoData } from "@/app/_types/pomodoro";
+
+/** ポモドーロ統計情報のレスポンス型定義 */
+export interface PomodoroStatsResponseType {
+  status: string;
+  message: string;
+  data: {
+    totalCompletedCount: number;
+    totalTime: number;
+    totalDays: number;
+    average: number;
+    weeklyData: PeriodicPomoData;
+    monthlyData: PeriodicPomoData;
+  };
+}
