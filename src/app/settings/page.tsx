b044f7800ebda3bcf3b4344660bab9aa5e606320
hayato33 <country.devide1933@gmail.com>
2025-03-29 00:36:50 +0900

    設定ページ実装 - 現在の設定データを元に初期値設定 - 各セクションコンポーネントにプロパティを渡すよう変更 - フォームを送信できるよう変更

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import PageTitle from "../_components/elements/PageTItle";
 import ProfileSection from "./_components/sections/ProfileSection";
 import TimerSection from "./_components/sections/TimerSection";
@@ -5,19 +7,60 @@ import SoundSection from "./_components/sections/SoundSection";
 import TimelineSection from "./_components/sections/TimelineSection";
 import RankingSection from "./_components/sections/RankingSection";
 import PreferenceSection from "./_components/sections/PreferenceSection";
-import ActionButtons from "./_components/ActionButtons";
+import FormActions from "./_components/FormActions";
+import { useUser } from "../_hooks/useUser";
+import { useSetting } from "../_hooks/useSetting";
+import { useForm } from "react-hook-form";
+import { UpdateData } from "./_types/updateData";
+import { useEffect } from "react";
+import { settingSchema } from "./_lib/settingSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useSupabaseSession } from "../_hooks/useSupabaseSession";
+import { submitHandler } from "./_lib/submitHandler";
+import { Form } from "../_components/Form";
 
 export default function Page() {
+  const { token } = useSupabaseSession();
+  const { data: userData } = useUser();
+  const user = userData?.data;
+  const { data: settingsData } = useSetting();
+  const settings = settingsData?.data;
+
+  const form = useForm<UpdateData>({
+    resolver: zodResolver(settingSchema),
+    mode: "onTouched",
+  });
+  const {
+    control,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = form;
+
+  useEffect(() => {
+    if (user || settings)
+      form.reset({
+        ...user,
+        ...settings,
+      });
+  }, [user, settings, form]);
+
+  const onSubmit = async (formData: UpdateData) =>
+    await submitHandler(formData, token || "");
+
   return (
     <div className="mx-auto max-w-[400px]">
       <PageTitle>各種設定</PageTitle>
-      <ProfileSection />
-      <TimerSection />
-      <SoundSection />
-      <TimelineSection />
-      <RankingSection />
-      <PreferenceSection />
-      <ActionButtons />
+      <Form {...form}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <ProfileSection control={control} isSubmitting={isSubmitting} />
+          <TimerSection control={control} isSubmitting={isSubmitting} />
+          <SoundSection control={control} isSubmitting={isSubmitting} />
+          <TimelineSection control={control} isSubmitting={isSubmitting} />
+          <RankingSection control={control} isSubmitting={isSubmitting} />
+          <PreferenceSection control={control} isSubmitting={isSubmitting} />
+          <FormActions isSubmitting={form.formState.isSubmitting} />
+        </form>
+      </Form>
     </div>
   );
 }
