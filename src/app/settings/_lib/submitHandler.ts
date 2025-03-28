60cf6ab9bd0492b2c416e0351088afd49ad31e72
hayato33 <country.devide1933@gmail.com>
2025-03-29 00:33:06 +0900

    設定ページのフォーム送信関数を作成

---
+++ src/app/settings/_lib/submitHandler.ts
@@ -0,0 +1,34 @@
+import { fetcher } from "@/app/_utils/fetcher";
+import { UpdateData } from "../_types/updateData";
+
+export const submitHandler = async (formData: UpdateData, token: string) => {
+  try {
+    const [userRes, settingRes] = await Promise.all([
+      fetcher({
+        apiPath: "/api/user",
+        method: "PUT",
+        body: { ...formData },
+        token: token || "",
+      }),
+      fetcher({
+        apiPath: "/api/setting",
+        method: "PUT",
+        body: { ...formData },
+        token: token || "",
+      }),
+    ]);
+
+    console.log("userRes", userRes);
+    console.log("settingRes", settingRes);
+
+    if (userRes.status !== "success" || settingRes.status !== "success") {
+      throw new Error("ユーザー情報またはユーザー設定の更新に失敗しました");
+    }
+
+    // 成功メッセージを表示
+    alert("設定を更新しました");
+  } catch (error) {
+    console.error("設定更新処理中にエラーが発生しました:", error);
+    alert("処理中にエラーが発生しました。もう一度お試しください。");
+  }
+};
