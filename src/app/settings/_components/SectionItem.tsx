caf891fcec975010dd43b0756e8c9375799c707f
hayato33 <country.devide1933@gmail.com>
2025-03-28 23:42:56 +0900

    エラーメッセージ表示用のErrorMessageコンポーネントを作成し、SectionItemコンポーネントにエラーメッセージ表示機能を追加

--- src/app/settings/_components/SectionItem.tsx
+++ src/app/settings/_components/SectionItem.tsx
@@ -1,18 +1,30 @@
 import { Label } from "@/app/_components/elements/Label";
+import { FieldErrors } from "react-hook-form";
+import { UpdateData } from "../_types/updateData";
+import { ErrorMessage } from "@/app/_components/elements/ErrorMessage";
 
 interface Props {
   labelText: string;
-  id: string;
+  id: keyof UpdateData;
   children: React.ReactNode;
+  errors: FieldErrors<UpdateData>;
 }
 
-export default function SectionItem({ labelText, id, children }: Props) {
+export default function SectionItem({
+  labelText,
+  id,
+  children,
+  errors,
+}: Props) {
   return (
-    <div className="flex flex-wrap items-center justify-between gap-2 sm:flex-row">
-      <Label htmlFor={id} className="min-w-36 font-normal">
-        {labelText}
-      </Label>
-      {children}
+    <div>
+      <div className="flex flex-wrap items-center justify-between gap-2 sm:flex-row">
+        <Label htmlFor={id} className="min-w-36 font-normal">
+          {labelText}
+        </Label>
+        {children}
+      </div>
+      <ErrorMessage message={errors[id]?.message} />
     </div>
   );
 }
