029b18e2ae1118a5747c520ae4c995783c67a432
hayato33 <country.devide1933@gmail.com>
2025-04-03 04:39:20 +0900

    shadcn/uiのDropdownMenuコンポーネントをインストール

---
+++ src/app/_components/DropdownMenu/index.ts
@@ -0,0 +1,42 @@
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuGroup,
+  DropdownMenuPortal,
+  DropdownMenuSub,
+  DropdownMenuRadioGroup,
+} from "./Root";
+
+import { DropdownMenuSubTrigger } from "./Trigger";
+
+import { DropdownMenuContent, DropdownMenuSubContent } from "./Content";
+
+import {
+  DropdownMenuItem,
+  DropdownMenuCheckboxItem,
+  DropdownMenuRadioItem,
+} from "./Item";
+
+import {
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+} from "./Label";
+
+export {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuCheckboxItem,
+  DropdownMenuRadioItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuGroup,
+  DropdownMenuPortal,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuRadioGroup,
+};
