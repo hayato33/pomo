c362a42862230e6740c8aa21d86d37c3ca0ce6d7
hayato33 <country.devide1933@gmail.com>
2025-04-03 16:56:34 +0900

    ランキング型定義において、日次・週次・月次・年次の各ランキングのデータをnumberのみに変更

--- src/app/_types/ranking.ts
+++ src/app/_types/ranking.ts
@@ -7,40 +7,40 @@ export interface Ranking {
 
 /** 日次ランキング(カウント) */
 export interface DailyRankingByCount extends Ranking {
-  daily_count: number | null;
+  daily_count: number;
 }
 
 /** 日次ランキング(時間) */
 export interface DailyRankingByTime extends Ranking {
-  daily_time: number | null;
+  daily_time: number;
 }
 
 /** 月次ランキング(カウント) */
 export interface MonthlyRankingByCount extends Ranking {
-  monthly_count: number | null;
+  monthly_count: number;
 }
 
 /** 月次ランキング(時間) */
 export interface MonthlyRankingByTime extends Ranking {
-  monthly_time: number | null;
+  monthly_time: number;
 }
 
 /** 週次ランキング(カウント) */
 export interface WeeklyRankingByCount extends Ranking {
-  weekly_count: number | null;
+  weekly_count: number;
 }
 
 /** 週次ランキング(時間) */
 export interface WeeklyRankingByTime extends Ranking {
-  weekly_time: number | null;
+  weekly_time: number;
 }
 
 /** 年次ランキング(カウント) */
 export interface YearlyRankingByCount extends Ranking {
-  yearly_count: number | null;
+  yearly_count: number;
 }
 
 /** 年次ランキング(時間) */
 export interface YearlyRankingByTime extends Ranking {
-  yearly_time: number | null;
+  yearly_time: number;
 }
