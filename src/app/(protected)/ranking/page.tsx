690d696b1b6c7c3aeb790d113dab93669ffb7ba8
hayato33 <country.devide1933@gmail.com>
2025-04-04 02:30:45 +0900

    count/time切り替えの状態管理ステートvalueType導入によりRankingTabsContentがシンプルになったため、page.tsx内で実装するよう変更

--- src/app/(protected)/ranking/page.tsx
+++ src/app/(protected)/ranking/page.tsx
@@ -3,11 +3,19 @@
 import PageTitle from "../../_components/elements/PageTItle";
 import { useRanking } from "../../_hooks/useRanking";
 import { RankingLoading } from "./_components/RankingLoading";
-import { Tabs, TabsList, TabsTrigger } from "@/app/_components/Tabs";
-import { RankingTabsContent } from "./_components/RankingTabsContent";
+import {
+  Tabs,
+  TabsList,
+  TabsTrigger,
+  TabsContent,
+} from "@/app/_components/Tabs";
+import { RankingTable } from "./_components/RankingTable";
 import { RankingData } from "./_types/rankingData";
+import { useState } from "react";
 
 export default function Page() {
+  const [valueType, setValueType] = useState<"count" | "time">("count");
+
   const { data: rankingData, isLoading, isError } = useRanking();
   const ranking = rankingData?.data;
   if (!ranking) return <div>ランキングデータが存在しません</div>;
@@ -39,26 +47,55 @@ export default function Page() {
                 <TabsTrigger value="monthly">月</TabsTrigger>
                 <TabsTrigger value="yearly">年</TabsTrigger>
               </TabsList>
-              <RankingTabsContent
-                period="daily"
-                rankingByCount={dailyRankingByCount}
-                rankingByTime={dailyRankingByTime}
-              />
-              <RankingTabsContent
-                period="weekly"
-                rankingByCount={weeklyRankingByCount}
-                rankingByTime={weeklyRankingByTime}
-              />
-              <RankingTabsContent
-                period="monthly"
-                rankingByCount={monthlyRankingByCount}
-                rankingByTime={monthlyRankingByTime}
-              />
-              <RankingTabsContent
-                period="yearly"
-                rankingByCount={yearlyRankingByCount}
-                rankingByTime={yearlyRankingByTime}
-              />
+              <div className="absolute right-4 top-2 z-10">
+                <Tabs
+                  value={valueType}
+                  onValueChange={(value) =>
+                    setValueType(value as "count" | "time")
+                  }
+                >
+                  <TabsList>
+                    <TabsTrigger value="count">回数</TabsTrigger>
+                    <TabsTrigger value="time">時間</TabsTrigger>
+                  </TabsList>
+                </Tabs>
+              </div>
+
+              {/* 日次ランキング */}
+              <TabsContent value="daily">
+                {valueType === "count" ? (
+                  <RankingTable data={dailyRankingByCount} dataType="count" />
+                ) : (
+                  <RankingTable data={dailyRankingByTime} dataType="time" />
+                )}
+              </TabsContent>
+
+              {/* 週次ランキング */}
+              <TabsContent value="weekly">
+                {valueType === "count" ? (
+                  <RankingTable data={weeklyRankingByCount} dataType="count" />
+                ) : (
+                  <RankingTable data={weeklyRankingByTime} dataType="time" />
+                )}
+              </TabsContent>
+
+              {/* 月次ランキング */}
+              <TabsContent value="monthly">
+                {valueType === "count" ? (
+                  <RankingTable data={monthlyRankingByCount} dataType="count" />
+                ) : (
+                  <RankingTable data={monthlyRankingByTime} dataType="time" />
+                )}
+              </TabsContent>
+
+              {/* 年次ランキング */}
+              <TabsContent value="yearly">
+                {valueType === "count" ? (
+                  <RankingTable data={yearlyRankingByCount} dataType="count" />
+                ) : (
+                  <RankingTable data={yearlyRankingByTime} dataType="time" />
+                )}
+              </TabsContent>
             </Tabs>
           </div>
         )}
