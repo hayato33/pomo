438917d8bb58d3781e5222af9d733fe1b2a74619
hayato33 <country.devide1933@gmail.com>
2025-04-26 01:17:17 +0900

    GetRankingResponseの変更に伴い、ランキング関連の型を修正

--- src/app/_hooks/useRanking.ts
+++ src/app/_hooks/useRanking.ts
@@ -1,9 +1,10 @@
 import { useGetEndpoint } from "./useGetEndpoint";
+import { GetRankingResponse } from "@/app/_types/ranking";
 
 /**
  * ランキング情報を取得するカスタムフック
  * @returns ランキング情報、ローディング状態、エラー状態、データ再取得関数
  */
 export const useRanking = () => {
-  return useGetEndpoint("/api/ranking");
+  return useGetEndpoint<GetRankingResponse>("/api/ranking");
 };
--- src/app/_types/ranking.ts
+++ src/app/_types/ranking.ts
@@ -6,3 +6,21 @@ export interface Ranking {
   rank: number;
   value: number;
 }
+
+export interface RankingData {
+  dailyRankingByCount: Ranking[];
+  dailyRankingByTime: Ranking[];
+  monthlyRankingByCount: Ranking[];
+  monthlyRankingByTime: Ranking[];
+  weeklyRankingByCount: Ranking[];
+  weeklyRankingByTime: Ranking[];
+  yearlyRankingByCount: Ranking[];
+  yearlyRankingByTime: Ranking[];
+}
+
+/** ランキング取得APIのレスポンス型定義 */
+export interface GetRankingResponse {
+  status: "success";
+  message: string;
+  data: RankingData;
+}
