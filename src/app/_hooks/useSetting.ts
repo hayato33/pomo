7ef439df5c16b8fcee303290a06597456489324f
hayato33 <country.devide1933@gmail.com>
2025-04-26 01:40:48 +0900

    GetRankingResponseの変更に伴い、ユーザー設定関連の型を修正

--- src/app/_hooks/useSetting.ts
+++ src/app/_hooks/useSetting.ts
@@ -1,9 +1,10 @@
 import { useGetEndpoint } from "./useGetEndpoint";
+import { SettingResponse } from "@/app/_types/setting";
 
 /**
  * ユーザー設定を取得するカスタムフック
  * @returns ユーザー設定、ローディング状態、エラー状態、データ再取得関数
  */
 export const useSetting = () => {
-  return useGetEndpoint("/api/setting");
+  return useGetEndpoint<SettingResponse>("/api/setting");
 };
--- src/app/_types/setting.ts
+++ src/app/_types/setting.ts
@@ -1,5 +1,13 @@
 import { z } from "zod";
 import { settingSchema } from "../(protected)/settings/_lib/settingFormSchema";
+import { UserSetting } from "@prisma/client";
 
 /** ユーザー設定の型(更新用) */
 export type UpdateUserSetting = z.infer<typeof settingSchema>;
+
+/** ユーザー設定取得・更新APIのレスポンス型定義 */
+export interface SettingResponse {
+  status: "success";
+  message: string;
+  data: UserSetting;
+}
