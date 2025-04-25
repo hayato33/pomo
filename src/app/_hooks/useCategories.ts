5f38cbc1e30df27649f5cfb37fa2325c5bf4c959
hayato33 <country.devide1933@gmail.com>
2025-04-26 00:29:06 +0900

    useCategoriesフックでuseGetEndpointの戻り値の型を指定

--- src/app/_hooks/useCategories.ts
+++ src/app/_hooks/useCategories.ts
@@ -1,9 +1,10 @@
 import { useGetEndpoint } from "./useGetEndpoint";
+import { GetCategoriesResponse } from "@/app/_types/category";
 
 /**
  * カテゴリ情報を取得するカスタムフック
  * @returns カテゴリ情報、ローディング状態、エラー状態、データ再取得関数
  */
 export const useCategories = () => {
-  return useGetEndpoint("/api/category");
+  return useGetEndpoint<GetCategoriesResponse>("/api/category");
 };
