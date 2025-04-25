3625a30a44de4679a0ec0fb416b57a3c6145a807
hayato33 <country.devide1933@gmail.com>
2025-04-26 01:47:47 +0900

    GetRankingResponseの変更に伴い、ユーザー情報関連の型を修正

--- src/app/_types/user.ts
+++ src/app/_types/user.ts
@@ -1,5 +1,13 @@
 import { z } from "zod";
 import { userSchema } from "../(protected)/settings/_lib/settingFormSchema";
+import { User } from "@prisma/client";
 
 /** ユーザー情報の型(更新用) */
 export type UpdateUser = z.infer<typeof userSchema>;
+
+/** ユーザー情報取得・更新APIのレスポンス型定義 */
+export interface UserResponse {
+  status: "success";
+  message: string;
+  data: User;
+}
